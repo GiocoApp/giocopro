fb0b492670ef9f4347e551ed9eb9baf9df9c9e24
Joao Moura <joaomdmoura@gmail.com>
2014-08-24 02:14:57 -0300

    updating api

--- lib/giocopro.rb
+++ lib/giocopro.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 class GiocoPro
   def initialize(token=false)
-    @http    = Net::HTTP.new('app.gioco.pro')
+    @http    = Net::HTTP.new('localhost:3000')
     @headers = {'Content-Type' =>'application/json', 'Token' =>(token) ? token : ENV['GIOCO_PRO_TOKEN']}
   end
 
@@ -11,35 +11,9 @@ class GiocoPro
     get_data('get_resource.json', aid)
   end
 
-  def add_badge(name, aid, badge_url = false)
-    data                              = {'badge' => {'name' => name}}
-    data['badge']['remote_image_url'] = badge_url if badge_url
-    post_data('add_badge.json', aid, data)
-  end
-
-  def remove_badge(bid, aid)
-    data = {'badge' => {'id' => bid}}
-    delete_data('remove_badge.json', aid, data)
-  end
-
-  def add_points(points, aid)
-    data = {'points' => {'points' => points}}
-    post_data('add_points.json', aid, data)
-  end
-
-  def remove_points(points, aid)
-    data = {'points' => {'points' => points}}
-    delete_data('remove_points.json', aid, data)
-  end
-
-  def add_level(levels, aid)
-    data = {'levels' => {'levels' => levels}}
-    post_data('add_level.json', aid, data)
-  end
-
-  def remove_level(levels, aid)
-    data = {'levels' => {'levels' => levels}}
-    delete_data('remove_level.json', aid, data)
+  def track_event(name, aid)
+    data                              = {'event' => {'name' => name}}
+    post_data('track_event.json', aid, data)
   end
 
   private
@@ -50,12 +24,12 @@ class GiocoPro
     data
   end
 
-  def get_data(url, aid, data = nil)
+  def get_data(url, aid)
     req = Net::HTTP::Get.new(('/api/' + url), @headers)
-    request!(data, aid, req)
+    request!(nil, aid, req)
   end
 
-  def post_data(url, aid, data = nil)
+  def post_data(url, aid, data)
     req = Net::HTTP::Post.new(('/api/' + url), @headers)
     request!(data, aid, req)
   end
